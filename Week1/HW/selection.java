7a6aad28f28f296a12bf55d48f27782cad635303
DeckaBro <keshawa.anuhas2003@gmail.com>
2022-09-25 22:41:50 +0530

    Uploaded the assesment

---
+++ Week1/HW/selection.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Selection{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String gender;
+        String color = "error";
+        boolean isBoy;
+        int age;
+        System.out.println("Are you a girl or a boy?");
+        gender = scn.nextLine();
+
+        System.out.println("Enter your Age: ");
+        age = scn.nextInt();
+
+        if((gender).equals("boy")){
+            if(age>15){
+                color = "Blue";
+            }else{
+                color = "Orange";
+            }
+        }else{
+            if(age>15){
+                color = "Yellow";
+            }else{
+                color = "Red";
+            }
+        }
+
+        System.out.println("Your color is: " + color);
+    }
+}
